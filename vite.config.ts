0c056ff4e923221b4781fdd73f6dc5e2dcf14acf
Tariq <tariqshariff.ts@gmail.com>
2023-03-12 21:23:20 +0530

    set base url

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/the-united-ramadan-drive/",
+  base: "/the-united-ramadan-23/",
   plugins: [react()],
 });
