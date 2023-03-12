80ceb8d7b28ee92bf08560dd6e014a670662f562
Tariq <tariqshariff.ts@gmail.com>
2023-03-12 21:11:00 +0530

    fix lint error

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,7 +1,6 @@
 import { FC } from "react";
-import BannerImage from "../../assets/banner.jpg";
 import { Image } from "antd";
 
 export const Banner: FC = () => {
-  return <Image src={BannerImage} />;
+  return <Image src="/banner.jpg" />;
 };
